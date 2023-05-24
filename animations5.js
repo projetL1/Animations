9a7b5cb219e8b9e3c8183473398ad5e0caae0100
projetL1 <132078493+projetL1@users.noreply.github.com>
2023-05-24 10:05:30 +0200

    Update animations5.js

--- animations5.js
+++ animations5.js
@@ -1,16 +1,19 @@
+//on signale a GSAP que on a importé un plugin ici "draggable"
 gsap.registerPlugin(Draggable);
+//on indique que le carre peut etre deplace
 Draggable.create(".carre", { 
     bounds: "body"
 });
-
-Draggable.create(".cercle", { 
-    inertia: true,
+//on indique que le cercle peut etre deplace et qu'il conserve 
+//aussi une inertie qui lui fait continuer
+//son mouvement meme apres le drag initiale à la souris
+Draggable.create(".carre2", { 
     bounds: "body",
     
+    
 });
-
-Draggable.create(".losange", {
+//type rotation => le type de mouvement n'est plus linéaire mais rotatif
+Draggable.create(".cercle", {
     bounds:"body",
     type:"rotation",
-    inertia:true,
 })
