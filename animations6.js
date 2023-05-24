cf8bf0b822722222302a4289d0e68017f521e271
projetL1 <132078493+projetL1@users.noreply.github.com>
2023-05-24 10:35:39 +0200

    Update animations6.js

--- animations6.js
+++ animations6.js
@@ -1,25 +1,38 @@
-document.querySelector(".uneBoule_avecFil1").addEventListener("click",boulier);
-
-
+//ecouteur d'evenement sur boulier
+document.querySelector(".uneBoule_avecFil1").addEventListener("click",boulier); 
+//boulier appel boulier1
 function boulier(){
-        console.log(i)
-        gsap.fromTo(".uneBoule_avecFil1",{
-            rotate:0,
-         },{
-            rotate:45,
-            duration:1,
-            yoyo:true,
-            repeat:1,
-            onComplete:() => {
-                gsap.fromTo(".uneBoule_avecFil2",{
-                    rotate:0,
-                },{
-                    rotate:-45,
-                    duration:1,
-                    yoyo:true,
-                    repeat:1,
-                });
-            },
-        
-        });     
+    console.log("itération")
+    boulier1()
 }
+//boulier 1 agit sur la premiere boule du boulier
+function boulier1(){
+    gsap.fromTo(".uneBoule_avecFil1",{
+        rotate:0,
+     },{
+        rotate:45,
+        duration:1,
+        yoyo:true,
+        repeat:1,
+        //fin de l'animation de la premiere boule on appel boulier2
+        onComplete:() => {
+            boulier2()
+        }
+    });
+}
+//boulier2 agit sur la dernière boule du boulier
+function boulier2(){
+    gsap.fromTo(".uneBoule_avecFil2",{
+        rotate:0,
+    },{
+        rotate:-45,
+        duration:1,
+        yoyo:true,
+        repeat:1,
+        //on rappel boulier => boucle infini
+        onComplete:() => {
+            boulier()
+        }
+    });
+}
+//l'animation s'arrete quand on recharge la page
